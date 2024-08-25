32bd8054896a0cf46a0beabb4c3a5989de68b367
Saif <saifakram947@gmail.com>
2024-08-25 11:21:46 +0530

    delete

---
+++ Graph/adjjacency_list.cpp
@@ -0,0 +1,64 @@
+// #include<iostream>
+// #include<vector>
+// #include<list>
+// using namespace std;
+// vector<list<int>>graph;
+// int v;
+// void add_edge(int src,int dest,bool bi_dir=true){
+//     graph[src].push_back(dest);
+//     if(bi_dir){
+//         graph[dest].push_back(src);
+//     }
+// }
+// void display(){
+//     for(int i=0;i<graph.size();i++){
+//         cout<<i<<" -> ";
+//         for(auto el:graph[i])cout<<el<<" , ";
+//         cout<<"\n";
+//     }
+// }
+// int main(){
+//     cin>>v;
+//     graph.resize(v,list<int>());
+//     int e;
+//     cin>>e;
+//     while(e--){
+//         int s,d;
+//         cin>>s>>d;
+//         add_edge(s,d,false);
+//     }
+//     display();
+//     return 0;
+// }
+#include<iostream>
+#include<vector>
+#include<list>
+using namespace std;
+vector<list<pair<int,int>>>graph;
+int v;
+void add_edge(int src,int dest,int wt,bool bi_dir=true){
+    graph[src].push_back({dest,wt});
+    if(bi_dir){
+        graph[dest].push_back({src,wt});
+    }
+}
+void display(){
+    for(int i=0;i<graph.size();i++){
+        cout<<i<<" -> ";
+        for(auto el:graph[i])cout<<el.first<<" "<<el.second<<" , ";
+        cout<<"\n";
+    }
+}
+int main(){
+    cin>>v;
+    graph.resize(v,list<pair<int,int>>());
+    int e;
+    cin>>e;
+    while(e--){
+        int s,d,wt;
+        cin>>s>>d>>wt;
+        add_edge(s,d,wt,false);
+    }
+    display();
+    return 0;
+}
